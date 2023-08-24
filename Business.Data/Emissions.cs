4d162e0fe43db8c075dbc63dd69ef5d6128489a9
Doug Revell <doug.revelljr@gmail.com>
2023-08-24 09:09:01 -0400

    Fixing Emissions

--- Business.Data/Emissions.cs
+++ Business.Data/Emissions.cs
@@ -7,66 +7,67 @@ namespace BusinessData
 
         // 2022 Moves Data
 
-        public double RunningNOX = 5.185261805;
-        public double IdleNOX = 50.16044454;
-        public double SmashNOX = 42.56005757;
-        public double SmashRunNOX = 2.441878983;
-        public double SmashIdleNOX = 34.04804606;
-
+        //Vehicle (Hauler) Running Emissions
+        public double RunningNOX = 5.815261805;
         public double RunningN20 = .003793665091;
-        public double IdleN20 = .08277499601;
-        public double SmashN20 = .1034848964;
-        public double SmashRunN20 = .00379520135;
-        public double SmashIdleN20 = .08278773965;
-
         public double RunningPM25 = .1758670473;
-        public double IdlePM25 = 1.77749758;
-        public double SmashPM25 = .0259291684;
-        public double SmashRunPM25 = .004990633014;
-        public double SmashIdlePM25 = .0207433472;
-
         public double RunningPM10 = .1911604372;
-        public double IdlePM10 = 1.932066948;
-        public double SmashPM10 = .02818391236;
-        public double SmashRunPM10 = .005424617031;
-        public double SmashIdlePM10 = .02254712989;
-
         public double RunningSO2 = .005905093448;
-        public double IdleSO2 = .02560554259;
-        public double SmashSO2 = .03034194794;
-        public double SmashRunSO2 = .005716274046;
-        public double SmashIdleSO2 = .02427355835;
-
         public double RunningCH4 = .03192717203;
-        public double IdleCH4 = .3635740162;
-        public double SmashCH4 = .3453954844;
-        public double SmashRunCH4 = .02148934923;
-        public double SmashidleCH4 = .2763163875;
-
         public double RunningCO = 2.502266751;
-        public double IdleCO = 18.62982519;
-        public double SmashCO = 16.98806791;
-        public double SmashRunCO = 1.693224025;
-        public double SmashIdleCO = 13.59045433;
-
         public double RunningVOC = .3611081042;
-        public double IdleVOC = 3.809808368;
-        public double SmashVOC = .7483118462;
-        public double SmashRunVOC = .09491856905;
-        public double SmashIdleVOC = .598649477;
-
         public double RunningCO2 = 1743.222553;
-        public double IdleCO2 = 7558.133922;
-        public double SmashCO2 = 9098.481403;
-        public double SmashRunCO2 = 1714.101577;
-        public double SmashIdleCO2 = 7278.785122;
-
         public double RunningCO2EQ = 1745.107042;
-        public double IdleCO2EQ = 7591.358337;
-        public double SmashCO2EQ = 9137.863419;
-        public double SmashRunCO2EQ = 1715.763589;
-        public double SmashIdleC02EQ = 7310.290735;
 
+        //Vehicle (Hauler) Idle Emissions
+        public double IdleNOX = 50.16044454;
+        public double IdleN20 = .08277499601;
+        public double IdlePM25 = 1.77749758;
+        public double IdlePM10 = 1.932066948;
+        public double IdleSO2 = .02560554259;
+        public double IdleCH4 = .3635740162;
+        public double IdleCO = 18.62982519;
+        public double IdleVOC = 3.809808368;
+        public double IdleCO2 = 7558.133922;
+        public double IdleCO2EQ = 7591.358337;
+
+        //Smash SmashEmissions
+        public double SmashNOX = 42.56005757;
+        public double SmashN20 = .1034848964;
+        public double SmashPM25 = .0259291684;
+        public double SmashPM10 = .02818391236;
+        public double SmashSO2 = .03034194794;
+        public double SmashCH4 = .3453954844;
+        public double SmashCO = 16.98806791;
+        public double SmashVOC = .7483118462;
+        public double SmashCO2 = 9098.481403;
+        public double SmashCO2EQ = 9137.863419;
+
+
+        //Smash Running Emissions
+        public double SmashRunNOX = 2.441878983;
+        public double SmashRunN20 = .00379520135;
+        public double SmashRunPM25 = .004990633014;
+        public double SmashRunPM10 = .005424617031;
+        public double SmashRunSO2 = .005716274046;
+        public double SmashRunCH4 = .02148934923;
+        public double SmashRunCO = 1.693224025;
+        public double SmashRunVOC = .09491856905;
+        public double SmashRunCO2 = 1714.101577;
+        public double SmashRunCO2EQ = 1715.763589;
+
+
+        //Smash Idle Emissions
+        public double SmashIdleNOX = 34.04804606;
+        public double SmashIdleN20 = .08278773965;
+        public double SmashIdlePM25 = .0207433472;
+        public double SmashIdlePM10 = .02254712989;
+        public double SmashIdleSO2 = .02427355835;
+        public double SmashidleCH4 = .2763163875;
+        public double SmashIdleCO = 13.59045433;
+        public double SmashIdleVOC = .598649477;
+        public double SmashIdleCO2 = 7278.785122;
+        public double SmashIdleC02EQ = 7310.290735;
 
 
     }
